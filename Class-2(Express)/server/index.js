2293f5600ff3cce7f003bea7bb4d4fec0d17514c
mrinal1224 <iammrinalkc@gmail.com>
2025-08-25 17:30:33 +0530

    push

--- Class-2(Express)/server/index.js
+++ Class-2(Express)/server/index.js
@@ -2,15 +2,35 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  { course_id: 1, name: "Java", instructor: "Piyush" },
+  { course_id: 2, name: "JavaScript", instructor: "Shiva" },
+  { course_id: 3, name: "DBMS", instructor: "Tejas" },
+];
+
 // Read
-app.get('/' , (req , res)=>{
-    res.send('This is my Server')
-})
+app.get("/", (req, res) => {
+  res.send("This is my Server");
+});
+// To get All courses
+app.get("/courses", (req, res) => {
+  // All the Courses
+  res.send(courses);
+});
+
+// to get a single course based on name or id
+app.get("/courses/:id", (req, res) => {
+  // All the Courses
+  console.log(req.params); // {id : 2}
+  let course = courses.find((course) => course.course_id === parseInt(req.params.id));
+
+  res.send(course);
+});
 
 // Create
 // app.post()
 
-// // update 
+// // update
 // app.put()
 
 // // delete
